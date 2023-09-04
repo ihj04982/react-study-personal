4aae2baf1d5dbe3f834fab2b774a6fe820bda035
Heejeong Lim <ihj04982@gmail.com>
2023-09-04 21:40:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,20 +11,19 @@ function Header(props) {
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topcis.length; i++) {
+    let t = props.topcis[i];
+    lis.push(
+      <li key={t.id}>
+        <a href={"/read/" + t.id}>{t.title}</a>
+      </li>
+    );
+  }
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="read/1">html</a>
-        </li>
-        <li>
-          <a href="read/2">css</a>
-        </li>
-        <li>
-          <a href="read/3">js</a>
-        </li>
-      </ol>
+      <ol>{lis}</ol>
     </nav>
   );
 }
@@ -39,10 +38,15 @@ function Article(props) {
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ];
   return (
     <div>
       <Header title="React"></Header>
-      <Nav></Nav>
+      <Nav topcis={topics}></Nav>
       <Article title="Welcome" body="Hello"></Article>
       <Article title="Heejeong" body="Lim"></Article>
     </div>
